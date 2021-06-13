198191434d603a3c0b851d6a8726059035bd2349
jmnote <opcore@gmail.com>
2021-06-13 10:00:00 +0000

    .

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+// src/main.js
+
+import Vue from 'vue'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
 
 new Vue({
-  render: h => h(App),
+  vuetify,
 }).$mount('#app')
