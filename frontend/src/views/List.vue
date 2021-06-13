2ca9be29e35418cb85970d7e41b969072dc5c225
jmnote <opcore@gmail.com>
2021-06-13 18:35:32 +0000

    .

--- frontend/src/views/List.vue
+++ frontend/src/views/List.vue
@@ -1,5 +1,87 @@
 <template>
-  <div class="list">
-    <h1>This is an list page</h1>
+  <div>
+    <v-row dense class="mt-2">
+      <v-col v-for="(item, i) in articles" :key="i" cols="12">
+        <v-card>
+          <v-card-text>
+            <div>
+              <b>{{ item.title }}</b>
+            </div>
+            {{ item.content }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      articles: [
+        {
+          title: "Hello 1----------------------------------",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello 2",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello 1",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello 2",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello 1",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello 2",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello 1",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello 2",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+        {
+          title: "Hello",
+          content:
+            "This pattern can be used when you need a toolbar, a responsive drawer and content to be displayed following the Material ",
+        },
+      ],
+    };
+  },
+};
+</script>
