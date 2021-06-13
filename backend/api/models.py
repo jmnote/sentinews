535c435abda4cddcc8fbe2f53fae79268a1a972a
jmnote <opcore@gmail.com>
2021-06-13 07:55:14 +0000

    .

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    url = models.CharField(max_length=80)
+    user_ip = models.CharField(max_length=16)
+    status = models.CharField(max_length=16)
+    pub_date = models.DateTimeField('date published')
+    title = models.CharField(max_length=200, null=True, blank=True, default=None) 
+    content = models.CharField(max_length=10000, null=True, blank=True, default=None)
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
