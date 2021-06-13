46e6513397d44fe3853b22ccd9a3c46456073211
jmnote <opcore@gmail.com>
2021-06-13 07:13:32 +0000

    ,

--- .container/1_build_base.sh
+++ .container/1_build_base.sh
@@ -9,9 +9,10 @@ cat <<'EOF' > Dockerfile
 FROM python:3.8-slim
 RUN set -x \
 && apt-get update && apt-get install -y \
-	curl \
-	nginx \
-	libmariadbclient-dev gcc \
+  git \
+  curl \
+  nginx \
+  libmariadbclient-dev gcc \
 && curl -sL https://github.com/krallin/tini/releases/download/v0.19.0/tini -o /tini \
 && chmod +x /tini \
 && curl -sL https://deb.nodesource.com/setup_12.x | bash - \
