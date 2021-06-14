f1fe7ea62fe6080187db2ae28f0c43c959fa65be
jmnote <opcore@gmail.com>
2021-06-14 17:09:11 +0000

    .

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,14 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    url = models.CharField(max_length=80)
+    url = models.CharField(max_length=80, unique=True)
     title = models.CharField(max_length=200) 
     desc = models.CharField(max_length=500)
     pub_date = models.DateTimeField('date published')
     user_ip = models.CharField(max_length=16, null=True, blank=True, default=None)
     status = models.CharField(max_length=16, null=True, blank=True, default=None)
     content = models.CharField(max_length=10000, null=True, blank=True, default=None)
+    comments = models.CharField(max_length=1000, null=True, blank=True, default=None)
+    polarity = models.FloatField(null=True, blank=True, default=None)
 
-class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    content = models.CharField(max_length=300)
+# class Comment(models.Model):
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+#     content = models.CharField(max_length=300)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.utils import timezone
 
 import json
 import requests
 from bs4 import BeautifulSoup
+
+from api.models import Article
+
+import logging
+logger = logging.getLogger('django')
 
 def search(request, keyword):
     r = requests.get(f'https://search.daum.net/search?w=news&q={keyword}')
@@ -22,8 +28,35 @@ def search(request, keyword):
     return JsonResponse({'list':articles})
 
 def register(request):
-    data = json.loads(request.body.decode('utf-8'))
-    #data.title
+    ## STEP 1
+    data = json.loads(request.body)
+    url = data['url']
+    title = data['title']
+    desc = data['desc']
+    pub_date = timezone.now()
+    articles = Article.objects.filter(url=url) 
+    if articles.exists():
+        a = articles.first()
+        a.url = url
+        a.title = title
+        a.desc = desc
+        a.pub_date = pub_date
+    else:
+        a = Article(url=url, title=title, desc=desc, pub_date=pub_date)
+    a.save()
+
+    ## STEP 2
+    # r = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
+    # soup = BeautifulSoup(r.text, 'html.parser')
+
+    # title = soup.find("meta", property="og:title")
+    # title = title["content"] if title else None
+
+    # content = soup.select_one('#harmonyContainer section')
+    # content = "".join([str(x) for x in content.contents])
+    # content = content.strip()
+    
+
     return JsonResponse(data)
 
 
