34515c7231f662be69af822b94c4ef2fed91c1c4
jmnote <opcore@gmail.com>
2021-06-15 10:18:51 +0000

    .

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,7 +7,7 @@ class Article(models.Model):
     url = models.CharField(max_length=80, unique=True)
     title = models.CharField(max_length=200) 
     desc = models.CharField(max_length=500)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(default=timezone.now)
     user_ip = models.CharField(max_length=32)
     content = models.CharField(max_length=10000)
     comments = models.CharField(max_length=1000)
